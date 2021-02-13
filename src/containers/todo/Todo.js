00423d40e4b539454d5f51b4fc57e3a1bc4af566
Roms <rom.pxz@gmail.com>
2021-02-13 10:49:19 +0800

    Updated with image

--- src/containers/todo/Todo.js
+++ src/containers/todo/Todo.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Base from '../../hoc/Base';
-import { API } from 'aws-amplify';
+import { API, Storage } from 'aws-amplify';
 import { listTodos } from '../../graphql/queries';
 import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from '../../graphql/mutations';
 
 
-const initialFormState = { name: '', description: '' };
+const initialFormState = { name: '', description: '', image: '' };
 
 function  Todo() {
 
@@ -18,12 +18,24 @@ function  Todo() {
 
     async function fetchTodos() {
         const apiData = await API.graphql({query: listTodos});
+        const todoFromApi = apiData.data.listTodos.items;
+        await Promise.all(todoFromApi.map(async todo => {
+            if (todo.image) {
+                const image = await Storage.get(todo.image);
+                todo.image = image;
+            }
+            return todo;
+        }))
         setTodos(apiData.data.listTodos.items);
     }
 
     async function createTodo() {
         if (!formData.name || !formData.description) return;
         await API.graphql({query: createTodoMutation, variables: { input: formData}});
+        if(formData.image) {
+            const image = await Storage.get(formData.image);
+            formData.image = image;
+        }
         setTodos([...todos, formData]);
         setFormData(initialFormState);
     }
@@ -32,6 +44,14 @@ function  Todo() {
         const newTodoArray = todos.filter(todo => todo.id !== id);
         setTodos(newTodoArray);
         await API.graphql({query: deleteTodoMutation, variables: { input: id}});
+    }
+
+    async function onChange(e) {
+        if (!e.target.files[0]) return;
+        const file = e.target.files[0];
+        setFormData({...formData, image: file.name});
+        await Storage.put(file.name, file);
+        fetchTodos();
     }
 
 
@@ -48,6 +68,10 @@ function  Todo() {
             placeholder="Todo Description"
             value={formData.description}
         />
+        <input
+            type="file"
+            onChange={onChange}
+        />
         <button onClick={createTodo}>Create</button>
         <div style={{ marginBottom: 30}}>
             {
@@ -56,6 +80,9 @@ function  Todo() {
                         <h2>{todo.name}</h2>
                         <p>{todo.description}</p>
                         <button onClick={() => deleteTodo(todo)}>Delete</button>
+                        {
+                            todo.image && <img src={todo.image} style={{ width: 400 }} alt={'quality-assurance'}/>
+                        }
                     </div>
                 ))
             }
